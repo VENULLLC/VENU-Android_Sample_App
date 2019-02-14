2a0b4b483ed4303bdaf298dceb6b53cf2013ce92
Faisal Ali <fali@lrsus.com>
2019-02-13 18:23:46 -0600

    Move to BroadcastReceiver. Push notification handling.

--- venusdkkotlinsample/src/main/java/com/lrsus/venusdkdemo/MainActivity.kt
+++ venusdkkotlinsample/src/main/java/com/lrsus/venusdkdemo/MainActivity.kt
@@ -6,127 +6,121 @@ package com.lrsus.venusdkdemo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
-import android.widget.CompoundButton
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
 import android.app.PendingIntent
 import android.bluetooth.le.AdvertiseCallback
+import android.content.*
 import android.os.Build
 import android.support.v4.app.NotificationCompat
+import android.support.v4.content.LocalBroadcastManager
+import android.view.View
+import android.widget.*
 import com.android.volley.Request
 import com.lrsus.venusdk.*
 import java.util.*
 
 
 class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
-
+    private val BRAND_ID = UUID.fromString("671d3a8e-ee94-4395-9177-d5382d75ff10")
     // Callback for services
-    private val mVENUSDKCallback : VENUCallback = VENUCallbackImplementation()
-
-    // Service binders
-    private var broadcastServiceBinder : VENUBroadcast.VENUBroadcastBinder? = null
-    private var rangeServiceServiceBinder : VENURange.VENURangeBinder? = null
+    private lateinit var mVENUSDKCallback : VENUCallback
 
     // Library services
-    private var broadcastService : VENUBroadcast? = null
-    private var rangeService : VENURange? = null
+//    private lateinit var broadcastService : VENUBroadcast
+//    private lateinit var rangeService : VENURange
 
     // UI elements
     private var broadcastIdView : TextView? = null
     private var broadcastToggle : ToggleButton? = null
+    private lateinit var requestServiceButton : Button
     private lateinit var rangeView : TextView
-    private lateinit var service : VENUService
+    private lateinit var venuService : VENUService
+    private lateinit var mobileId : UUID
 
 
     /**
      * Set up service connection so we can set up the callbacks for VENURange
      */
-    private val mRangeConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            Log.d(MainApplication.TAG, "Broadcasting service connected")
-
-            rangeServiceServiceBinder = service  as VENURange.VENURangeBinder
-            rangeService = rangeServiceServiceBinder?.getService()
-
-            // Prevent ranging if we're already broadcasting (In the case of manual broadcasting)
-            if (broadcastService == null || broadcastService?.isBroadcasting() == false) {
-
-                // Set up callbacks
-                rangeService?.setCallback(mVENUSDKCallback)
-
-                // Start the service if it's not already running
-                if (!rangeService!!.isRunning()) {
-                    val rangeIntent = Intent(this@MainActivity, RangeService::class.java)
-                    startService(rangeIntent)
-                }
-            }
-        }
-
-        override fun onServiceDisconnected(p0: ComponentName?) {
-            Log.d(MainApplication.TAG, "Broadcasting service disconnected")
-        }
-
-    }
+//    private val mRangeConnection = object : ServiceConnection {
+//        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+//            Log.d(MainApplication.TAG, "Broadcasting service connected")
+//
+//            rangeServiceServiceBinder = service  as VENURange.VENURangeBinder
+//            rangeService = rangeServiceServiceBinder?.getService()
+//
+//            // Prevent ranging if we're already broadcasting (In the case of manual broadcasting)
+//            if (broadcastService == null || broadcastService?.isBroadcasting() == false) {
+//
+//                // Set up callbacks
+//                rangeService?.setCallback(mVENUSDKCallback)
+//
+//                // Start the service if it's not already running
+//                if (!rangeService!!.isRunning()) {
+//                    val rangeIntent = Intent(this@MainActivity, RangeService::class.java)
+//                    startService(rangeIntent)
+//                }
+//            }
+//        }
+//
+//        override fun onServiceDisconnected(p0: ComponentName?) {
+//            Log.d(MainApplication.TAG, "Broadcasting service disconnected")
+//        }
+//
+//    }
 
     /**
      * Set up service connection so we can set up the callbacks for VENUBroadcast
      * and to enable the foreground notification
      */
-    private val mBroadcastConnection = object : ServiceConnection {
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            Log.d(MainApplication.TAG, "Broadcast service disconnected")
-        }
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            Log.d(MainApplication.TAG, "Broadcast service connected")
-
-            broadcastServiceBinder = service as VENUBroadcast.VENUBroadcastBinder?
-            broadcastService = broadcastServiceBinder?.getService()
-
-            // Build notification for foreground service
-            val builder = NotificationCompat.Builder(applicationContext, MainApplication.APP_NAMESPACE)
-            builder.setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentTitle("Broadcasting your location.")
-                    .setOngoing(true)
-                    // Add clear action to stop the broadcasting service
-                    .addAction(
-                            android.R.drawable.ic_notification_clear_all,
-                            "Clear",
-                            broadcastService?.stopPendingIntent(applicationContext)
-                    )
-
-            // Reopen this activity when the foreground notification is tapped
-            val intent = Intent(this@MainActivity, MainActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(
-                    this@MainActivity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-            )
-            builder.setContentIntent(pendingIntent)
-
-            // Enable foreground notification
-            broadcastService?.setForegroundNotification(62362, builder.build())
-
-            // Connect the callback handlers.
-            broadcastService?.setCallback(mVENUSDKCallback)
-
-            // Check for an already running broadcast in the case we come back to the activity
-            // while the broadcasting is in the forground
-            if (broadcastService?.isBroadcasting() == true) {
-                // Update UI state accordingly
-                broadcastIdView?.text = broadcastService?.serviceNumber().toString()
-                broadcastToggle?.isChecked = true
-            }
-        }
-    }
+//    private val mBroadcastConnection = object : ServiceConnection {
+//
+//        override fun onServiceDisconnected(name: ComponentName?) {
+//            Log.d(MainApplication.TAG, "Broadcast service disconnected")
+//        }
+//
+//        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+//            Log.d(MainApplication.TAG, "Broadcast service connected")
+//
+////            broadcastServiceBinder = service as VENUBroadcast.VENUBroadcastBinder?
+////            broadcastService = broadcastServiceBinder?.getService()
+//
+//            // Build notification for foreground service
+//            val builder = NotificationCompat.Builder(applicationContext, MainApplication.APP_NAMESPACE)
+//            builder.setSmallIcon(R.mipmap.ic_launcher)
+//                    .setContentTitle("Broadcasting your location.")
+//                    .setOngoing(true)
+//                    // Add clear action to stop the broadcasting service
+//                    .addAction(
+//                            android.R.drawable.ic_notification_clear_all,
+//                            "Clear",
+//                            broadcastService?.stopPendingIntent(applicationContext)
+//                    )
+//
+//            // Reopen this activity when the foreground notification is tapped
+//            val intent = Intent(this@MainActivity, MainActivity::class.java)
+//            val pendingIntent = PendingIntent.getActivity(
+//                    this@MainActivity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+//            )
+//            builder.setContentIntent(pendingIntent)
+//
+//            // Enable foreground notification
+//            broadcastService?.setForegroundNotification(62362, builder.build())
+//
+//            // Connect the callback handlers.
+//            broadcastService?.setCallback(mVENUSDKCallback)
+//
+//            // Check for an already running broadcast in the case we come back to the activity
+//            // while the broadcasting is in the forground
+//            if (broadcastService?.isBroadcasting() == true) {
+//                // Update UI state accordingly
+//                broadcastIdView?.text = broadcastService?.serviceNumber().toString()
+//                broadcastToggle?.isChecked = true
+//            }
+//        }
+//    }
 
     // Copied from Android documentation:
     // https://developer.android.com/training/notify-user/build-notification
@@ -151,14 +145,23 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mVENUSDKCallback = VENUCallbackImplementation()
+        val uuid = MobileId.get(applicationContext)
+        if (uuid != null) {
+            mobileId = uuid
+        }
+
         broadcastIdView = findViewById(R.id.broadcastIdView) as TextView
         broadcastToggle = findViewById(R.id.broadcastSwitch) as ToggleButton
+        requestServiceButton = findViewById(R.id.requestServiceButton) as Button
         rangeView = findViewById(R.id.beaconDistance)
-        service = VENUService.getInstance(
+        venuService = VENUService.getInstance(
                 this,
                 getString(R.string.APP_ID).replace("\n", ""),
-                getString(R.string.APP_SECRET).replace("\n", "")
+                getString(R.string.APP_SECRET).replace("\n", ""),
+                mobileId
         )
+
         createNotificationChannel()
     }
 
@@ -170,59 +173,74 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
         MainApplication.setActivityRunStatus(true)
 
         // Bind VENUBroadcast
-        bindService(
-                Intent(this@MainActivity, VENUBroadcast::class.java),
-                mBroadcastConnection,
-                Context.BIND_AUTO_CREATE)
-
-        // Bind RangeService which extends VENURange
-        bindService(
-                Intent(this@MainActivity, RangeService::class.java),
-                mRangeConnection,
-                Context.BIND_AUTO_CREATE)
+//        bindService(
+//                Intent(this@MainActivity, VENUBroadcast::class.java),
+//                mBroadcastConnection,
+//                Context.BIND_AUTO_CREATE)
+//
+//        // Bind RangeService which extends VENURange
+//        bindService(
+//                Intent(this@MainActivity, RangeService::class.java),
+//                mRangeConnection,
+//                Context.BIND_AUTO_CREATE)
 
         broadcastToggle?.setOnCheckedChangeListener(this)
+
+//        requestServiceButton.setOnClickListener {
+//            venuService.serviceNumber(this.applicationContext)
+//        }
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mVENUSDKCallback, IntentFilter("VENU"))
+        startService(Intent(this@MainActivity, RangeService::class.java))
+        startService(Intent(this@MainActivity, VENUMessagingService::class.java))
     }
 
     override fun onStop() {
         super.onStop()
 
-        if (broadcastService == null || !broadcastService!!.isBroadcasting()) {
-            // Mark activity as not running to restart notifications but only if
-            // we're no longer broadcasting.
-            MainApplication.setActivityRunStatus(false)
-        }
+//        if (broadcastService == null || !broadcastService!!.isBroadcasting()) {
+//            // Mark activity as not running to restart notifications but only if
+//            // we're no longer broadcasting.
+//            MainApplication.setActivityRunStatus(false)
+//        }
 
-        if (rangeService!!.isRunning()) {
-            stopService(Intent(this@MainActivity, RangeService::class.java))
-        }
+//        if (rangeService!!.isRunning()) {
+//            stopService(Intent(this@MainActivity, RangeService::class.java))
+//        }
 
-        unbindService(mBroadcastConnection)
-        unbindService(mRangeConnection)
+//        unbindService(mBroadcastConnection)
+//        unbindService(mRangeConnection)
+        stopService(Intent(this@MainActivity, RangeService::class.java))
+        stopService(Intent(this@MainActivity, VENUMessagingService::class.java))
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mVENUSDKCallback)
+        super.onDestroy()
     }
 
     override fun onCheckedChanged(button : CompoundButton?, isChecked : Boolean) {
-        if (isChecked) {
-            if (broadcastService != null) {
-                // Manual broadcasting. Generate a random service number
-                val manualServiceNumber = 100 + Random().nextInt(154)
-
-                val broadcastIntent = Intent(this@MainActivity, VENUBroadcast::class.java)
-                broadcastIntent.putExtra("ServiceNumber", manualServiceNumber)
-
-                startService(broadcastIntent)
-            } else {
-                Toast.makeText(applicationContext, "Not Ready", Toast.LENGTH_SHORT)
-                button?.isChecked = false
-            }
-
-        } else {
-            if (broadcastService!!.isBroadcasting()) {
-                broadcastService?.stop()
-            }
-
-            broadcastIdView?.text = ""
-        }
+//        if (isChecked) {
+////            if (broadcastService != null) {
+//                // Manual broadcasting. Generate a random service number
+//                val manualServiceNumber = 100 + Random().nextInt(154)
+//
+//                val broadcastIntent = Intent(this@MainActivity, VENUBroadcast::class.java)
+//                broadcastIntent.putExtra("ServiceNumber", manualServiceNumber)
+//
+//                startService(broadcastIntent)
+//            } else {
+//                Toast.makeText(applicationContext, "Not Ready", Toast.LENGTH_SHORT)
+//                button?.isChecked = false
+//            }
+//
+//        } else {
+//            if (broadcastService!!.isBroadcasting()) {
+//                broadcastService?.stop()
+//            }
+//
+//            broadcastIdView?.text = ""
+//        }
     }
 
 
@@ -230,23 +248,64 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
      * The callback provides location and status updates.
      */
     inner class VENUCallbackImplementation : VENUCallback() {
+        override fun onServiceRequested(serviceNumber: VENUServiceNumber) {
+            // Service number has been accepted by server, but not by location.
+            runOnUiThread {
+                requestServiceButton.isEnabled = false
+                requestServiceButton.text = "Waiting..."
+            }
+        }
+
+        override fun onServiceExpiration(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceAccepted(serviceNumber: VENUServiceNumber) {
+            runOnUiThread {
+                requestServiceButton.isEnabled = false
+                requestServiceButton.text = serviceNumber.number.toString()
+            }
+        }
+
+        override fun onServiceStateChanged(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceLocated(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceOrderStarted(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceOrderPaged(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceCleared(serviceNumber: VENUServiceNumber) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun venuService(): VENUService {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
 
         /**
          * Provides location event when detected within a 5-10 meters of the locator/beacon.
          */
-        override fun enteredVENULocation(serviceNumber: Int, locationId : Int, distance : Double, initial : Boolean) {
+        override fun enteredVENULocation(locationId : UUID, distance : Double, initial : Boolean) {
             Toast.makeText(applicationContext, "Discovered VENU location", Toast.LENGTH_SHORT).show()
 
             runOnUiThread {
                 rangeView.text = String.format("%.2f", distance)
             }
-
-            if (initial && !broadcastService!!.isBroadcasting()) {
-                // Create an intent with service number generated to VENUBroadcast service
-                val broadcastIntent = Intent(this@MainActivity, VENUBroadcast::class.java)
-                broadcastIntent.putExtra("ServiceNumber", serviceNumber)
-                startService(broadcastIntent)
-            }
+//            if (initial && !broadcastService!!.isBroadcasting()) {
+//                // Create an intent with service number generated to VENUBroadcast service
+//                val broadcastIntent = Intent(this@MainActivity, VENUBroadcast::class.java)
+//                broadcastIntent.putExtra("ServiceNumber", serviceNumber)
+//                startService(broadcastIntent)
+//            }
         }
 
         /**
@@ -254,9 +313,9 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
          */
         override fun exitedVENULocation() {
             Toast.makeText(applicationContext, "Exited VENU location", Toast.LENGTH_SHORT).show()
-            if (broadcastService!!.isBroadcasting() == true) {
-                broadcastService?.stop()
-            }
+//            if (broadcastService!!.isBroadcasting() == true) {
+//                broadcastService?.stop()
+//            }
         }
 
         /**
